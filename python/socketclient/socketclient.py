de76514803b2aa414a2b01d2c97c830aae363391
Richard L Ford <richardlford@gmail.com>
2020-05-10 13:56:36 -0400

    In sample client, take target as arg1

--- python/socketclient/socketclient.py
+++ python/socketclient/socketclient.py
@@ -1,8 +1,8 @@
 import socket
 import sys
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
+HOST, PORT = sys.argv[1], 9999
+data = " ".join(sys.argv[2:])
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
